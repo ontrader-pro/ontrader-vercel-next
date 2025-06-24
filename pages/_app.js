a7a4629bb19860b2f13aa27ec1d4fd379d28ae36
ontrader <traderexitoso@gmail.com>
2025-06-23 23:10:58 -0500

    Tu mensaje de commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,14 @@
 // pages/_app.js
-import '../styles/globals.css';   // o la ruta donde tengas tus estilos globales
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
-// pages/_app.js
 import '../styles/globals.css';
-+ import { SpeedInsights } from '@vercel/speed-insights';
+import { SpeedInsights } from '@vercel/speed-insights';
 
 function MyApp({ Component, pageProps }) {
-+   // Este componente recoge métricas y las envía a tu dashboard de Vercel
-+   return (
-+     <>
-+       <SpeedInsights />
-+       <Component {...pageProps} />
-+     </>
-+   );
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp;
